4580d8e2e745d8e1f33886d9eba9a78324616b2f
Iswarya-e <42031606+Iswarya-e@users.noreply.github.com>
2023-08-22 22:11:52 +0530

    Create NumberOfIslands.cpp

---
+++ NumberOfIslands.cpp
@@ -0,0 +1,53 @@
+class Solution {
+ private:
+    
+ void BFS(int row,int col,vector<vector<int>> &vis,vector<vector<char>> &grid)
+ {
+    vis[row][col]=1;
+    queue<pair<int,int>> q;
+    q.push({row,col});
+    int maxr=grid.size();
+    int maxc=grid[0].size();
+    while(!q.empty())
+    {
+        int r=q.front().first;
+        int c=q.front().second;
+        q.pop();
+        for(int i=-1;i<=1;i++)
+        {
+            for(int j=-1;j<=1;j++)
+            {
+                if((abs(i)==abs(j))) continue;
+                int currrow=r+i;
+                int currcol=c+j;
+                if(currrow>=0 && currcol>=0 && currrow<maxr && currcol<maxc 
+                && grid[currrow][currcol]=='1' && !vis[currrow][currcol])
+                {
+                    vis[currrow][currcol]=1;
+                    q.push({currrow,currcol});
+                }
+            }
+        }
+            
+    }
+}
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int row=grid.size();
+        int col=grid[0].size();
+        vector<vector<int>> vis(row,vector<int>(col,0));
+        int count=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(!vis[i][j] && grid[i][j]=='1') // remove this for checking neughbours from 8 directions
+                {
+                    count++;
+                    BFS(i,j,vis,grid);
+                }
+            }
+        }
+        return count;
+    }
+};
