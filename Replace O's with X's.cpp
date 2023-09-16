a855fcf5ffd1e4df3ccd085c40b67893696b3a71
Iswarya-e <42031606+Iswarya-e@users.noreply.github.com>
2023-09-16 21:02:09 +0530

    Update Replace O's with X's.cpp

--- Replace O's with X's.cpp
+++ Replace O's with X's.cpp
@@ -1,83 +1,59 @@
-//TC
-O(NXM)
-	
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+    void DFSXO(vector<vector<int>> &visited,vector<vector<char>> &board,int r,int c)
+    {
+        int m=board.size();
+        int n=board[0].size();
+        visited[r][c]=1;
+        int dr[]={0,0,1,-1};
+        int dc[]={1,-1,0,0};
+        for(int i=0;i<4;i++)
+        {
+            int nr=r+dr[i];
+            int nc=c+dc[i];
+            if(nr>=0 && nc>=0 && nr<m && nr<n && visited[nr][nc]==0 && board[nr][nc]=='O')
+            {
+                DFSXO(visited,board,nr,nc);
+            }
+        }
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        int m=board.size();
+        int n=board[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            if(board[i][0]=='O' && visited[i][0]==0)
+            {
+                  DFSXO(visited,board,i,0);
+            }
+            if(board[i][n-1]=='O' && visited[i][n-1]==0)
+            {
+                DFSXO(visited,board,i,n-1);  
+            }
+        }
+       
+        for(int i=0;i<n;i++)
+        {
+            if(board[0][i]=='O' && visited[0][i]==0)
+            {
+                DFSXO(visited,board,0,i);  
+            }
+            if(board[m-1][i]=='O' && visited[m-1][i]==0)
+            {
+                 DFSXO(visited,board,m-1,i);
+            }
+        }
 
-// } Driver Code Ends
-class Solution 
-{
-    public:
-    //Function to find distance of nearest 1 in the grid for each cell.
-	vector<vector<int>>nearest(vector<vector<int>>grid)
-	{
-	    // Code here
-	    int m=grid.size();
-	    int n=grid[0].size();
-	    vector<vector<int>> visited(m,vector<int>(n,0));
-	    vector<vector<int>> ans(m,vector<int>(n,0));
-	    queue<pair<pair<int,int>,int>> q;
-	    for(int i=0;i<m;i++)
-	    {
-	        for(int j=0;j<n;j++)
-	        {
-	            if(grid[i][j]==1)
-	            {
-	                q.push({{i,j},0});
-	                ans[i][j]=0;
-	                visited[i][j]=1;
-	            }
-	        }
-	    }
-	   int dr[]={1,-1,0,0};
-	   int dc[]={0,0,1,-1};
-	    while(!q.empty())
-	    {
-	        int r=q.front().first.first;
-	        int c=q.front().first.second;
-	        int dis=q.front().second;
-	        q.pop();
-	        ans[r][c]=dis;
-	        for(int k=0;k<4;k++)
-	        {
-	            int newrow=r+dr[k];
-	            int newcol=c+dc[k];
-	            if(newrow>=0 && newrow<m and newcol>=0 && newcol<n && visited[newrow][newcol]!=1)
-	            {
-	                visited[newrow][newcol]=1;
-	                q.push({{newrow,newcol},dis+1});
-	            }
-	        }
-	        
-	        
-	    }
-	    return ans;
-	}
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(visited[i][j]==0 && board[i][j]=='O')
+                {
+                    board[i][j]='X';
+                }
+            }
+        }
+    }
 };
-
-//{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>grid(n, vector<int>(m, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				cin >> grid[i][j];
-			}
-		}
-		Solution obj;
-		vector<vector<int>> ans = obj.nearest(grid);
-		for(auto i: ans){
-			for(auto j: i){
-				cout << j << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
-}
-// } Driver Code Ends
